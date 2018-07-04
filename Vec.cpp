6c465983434a7cbcdae65b82cb02fe36e4586d84
JuliaHolk <40289425+JuliaHolk@users.noreply.github.com>
2018-07-04 15:48:07 +0200

    Vector Class (?)

---
+++ Vec.cpp
@@ -0,0 +1,81 @@
+//
+// Created by julia on 04.07.18.
+//
+
+#include "Vec.h"
+#include <math.h>
+
+//default values 0
+Vector::Vector() {
+    x=0;
+    y=0;
+    z=0;
+}
+
+Vector::Vector(double valx, double valy, double valz) {
+    x=valx;
+    y=valy;
+    z=valz;
+}
+
+int Vector::setx(double valx) {
+    x=valx;
+    return 0;
+}
+
+int Vector::sety(double valy) {
+    y=valy;
+    return 0;
+}
+
+int Vector::setz(double valz) {
+    z=valz;
+    return 0;
+}
+
+double Vector::getx() {
+    return x;
+}
+
+double Vector::gety() {
+    return y;
+}
+
+double Vector::getz() {
+    return z;
+}
+
+double Vector::lsq() {
+    double ls=x*x+y*y+z*z;
+    return ls;
+}
+
+double Vector::length() {
+    double l=sqrt(lsq());
+    return l;
+}
+
+Vector Vector::add(Vector &vec) {
+    Vector newvec(x+vec.getx(), y+vec.gety(), z+vec.getz());
+    return newvec;
+}
+
+Vector Vector::sub(Vector &vec) {
+    Vector newvec(x-vec.getx(), y-vec.gety(), z-vec.getz());
+    return newvec;
+}
+
+Vector Vector::mult(double a) {
+    Vector newvec(a*x, a*y, a*z);
+    return newvec;
+}
+
+double Vector::dotp(Vector &vec) {
+    double product=x*vec.getx()+y*vec.gety()+z*vec.getz();
+    return product;
+}
+
+Vector Vector::crossp(Vector &vec) {
+    Vector newvec(y*vec.getz()-z*vec.gety(), z*vec.getx()-x*vec.getz(), x*vec.gety()-y*vec.getx());
+    return newvec;
+}
---
+++ Vec.h
@@ -0,0 +1,57 @@
+//
+// Created by julia on 04.07.18.
+//
+
+#ifndef ZAPHOD_MASTER_VEC_H
+#define ZAPHOD_MASTER_VEC_H
+
+
+class Vector {
+public:
+    double x, y, z;
+
+    //constructors
+
+    //empty
+    Vector();
+
+    //values
+    Vector(double valx, double valy, double valz);
+
+    //methods
+
+    //set
+    int setx(double valx);
+    int sety(double valy);
+    int setz(double valz);
+
+    //get
+    double getx();
+    double gety();
+    double getz();
+
+    //squared length
+    double lsq();
+
+    //length
+    double length();
+
+    //addition
+    Vector add(Vector& vec);
+
+    //subtraction
+    Vector sub(Vector& vec);
+
+    //multiplication with scalar
+    Vector mult(double a);
+
+    //dot product
+    double dotp(Vector &vec);
+
+    //cross product
+    Vector crossp(Vector &vec);
+};
+
+
+
+#endif //ZAPHOD_MASTER_VEC_H
