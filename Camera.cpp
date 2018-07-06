9c03e0047e2211cd1f77a99766e1f91fa5ca2085
irini <irini2501@web.de>
2018-07-06 08:38:47 +0200

    Camera Class

---
+++ Camera.cpp
@@ -0,0 +1,11 @@
+//
+// Created by irini on 05.07.18.
+//
+
+#include "Camera.h"
+#include "Vec.h"
+#include "Colour.h"
+
+Camera::Camera() {}
+
+
---
+++ Camera.h
@@ -0,0 +1,15 @@
+//
+// Created by irini on 05.07.18.
+//
+
+#ifndef ZAPHOD_MASTER_CAMERA_H
+#define ZAPHOD_MASTER_CAMERA_H
+
+class Camera{
+       // Augpunkt
+       // Ansichtspunkt/Zentrum
+       // Ausdehnung
+};
+
+
+#endif //ZAPHOD_MASTER_CAMERA_H
