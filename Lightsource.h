a86ddfbd7a32d075e199a5094f03022481029dd2
irini <irini2501@web.de>
2018-07-09 16:06:38 +0200

    Lightsource class (wip)

---
+++ Lightsource.h
@@ -0,0 +1,20 @@
+//
+// Created by irini on 09.07.18.
+//
+
+#ifndef ZAPHOD_MASTER_LIGHTSOURCE_H
+#define ZAPHOD_MASTER_LIGHTSOURCE_H
+
+#include "Colour.h"
+#include "Vec.h"
+
+class Lightsource{
+    private:
+        Colour colour; //Colour Lightsource
+        Vec3D lightPosition; //Position Lightsource
+
+    public:
+    
+};
+
+#endif //ZAPHOD_MASTER_LIGHTSOURCE_H
